2978b0a1f4fe674bd4946406b7f9cb670aa328bc
Aryan Chakravarty <aryanchakravarty04@gmail.com>
2025-06-03 17:01:17 +0530

    form added using formspree

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -78,7 +78,7 @@ export const ContactSection = () => {
             <div className="bg-card p-8 rounded-lg shadow-xs">
                 <h3 className="text-2xl font-semibold mb-6"> Send a Message</h3>
 
-                <form className="space-y-6">
+                <form action="https://formspree.io/f/xyzjyope" method="POST" className="space-y-6">
                     <div>
                         <label htmlFor="name" className="block text-sm font-medium mb-2">
                             Your Name
